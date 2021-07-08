ce49e78dbb2376ce3dee5e4ecb6edb5ab3236c4e
Willy Kloucek <wkloucek@owncloud.com>
2021-07-08 09:00:56 +0200

    disable update checker and use qnap license

--- v20.04/overlay/etc/owncloud.d/39-qnap.sh
+++ v20.04/overlay/etc/owncloud.d/39-qnap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Enabling qnap app..."
 occ app:enable -n qnap
+occ config:system:set license-class --type string --value='\OCA\QNAP\QnapLicense'
 
 true
--- v20.04/overlay/etc/owncloud.d/40-qnap-config.sh
+++ v20.04/overlay/etc/owncloud.d/40-qnap-config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Setting qnap specific settings..."
+occ config:system:set updatechecker --type boolean --value=false
 occ config:app:set --value true core umgmt_show_is_enabled
 occ config:app:set --value true core umgmt_show_last_login
 occ config:system:set grace_period.demo_key.show_popup --type=boolean --value=false
